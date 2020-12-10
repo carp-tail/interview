ff9f5b209f662035a64b0d3fd2c8555d8e5c13ae
liwei <2750181390@qq.com>
2020-12-10 10:38:06 +0800

    打印ABC线程问题

--- src/test/java/com/guaniu/MyTest.java
+++ src/test/java/com/guaniu/MyTest.java
@@ -1,18 +1,6 @@
 package com.guaniu;
 
-import com.guaniu.chaper1.question3.RecursiveFunReverseStack;
-
-import java.util.Stack;
-
 public class MyTest {
     public static void main(String[] args){
-        Stack<Integer> stack = new Stack<Integer>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        RecursiveFunReverseStack qq = new RecursiveFunReverseStack();
-        qq.reverse(stack);
     }
 }
